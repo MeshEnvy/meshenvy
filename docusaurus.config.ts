7d324edb1c9909e21fc19488647a9d8a560d8f97
Ben Allfree <ben@benallfree.com>
2025-10-08 17:27:26 -0700

    docs and blog cleanup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,12 +39,12 @@ const config: Config = {
     [
       "classic",
       {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/benallfree/meshenvy/tree/main/docs",
-        },
+        // docs: {
+        //   sidebarPath: "./sidebars.ts",
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl: "https://github.com/benallfree/meshenvy/tree/main/docs",
+        // },
         blog: {
           showReadingTime: true,
           feedOptions: {
@@ -79,32 +79,32 @@ const config: Config = {
         src: "/favicon-32x32.png",
       },
       items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Tutorial",
-        },
-        { to: "/blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/benallfree/meshenvy",
-          label: "GitHub",
-          position: "right",
-        },
+        // {
+        //   type: "docSidebar",
+        //   sidebarId: "tutorialSidebar",
+        //   position: "left",
+        //   label: "Tutorial",
+        // },
+        // { to: "/blog", label: "Blog", position: "left" },
+        // {
+        //   href: "https://github.com/benallfree/meshenvy",
+        //   label: "GitHub",
+        //   position: "right",
+        // },
       ],
     },
     footer: {
       style: "dark",
       links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Tutorial",
+        //       to: "/docs/intro",
+        //     },
+        //   ],
+        // },
         {
           title: "Community",
           items: [
@@ -115,15 +115,6 @@ const config: Config = {
             {
               label: "X",
               href: "https://x.com/meshenvy",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog",
             },
             {
               label: "GitHub",
