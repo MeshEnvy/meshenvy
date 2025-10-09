c566f8235a2a837aca48b21faf1cace7b8baa80f
Ben Allfree <ben@benallfree.com>
2025-10-08 17:27:32 -0700

    youtube tutorial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://www.youtube.com/watch?v=gH-K9fRuhfQ">
+            Watch a Demo - Get Started with Meshtastic
           </Link>
         </div>
       </div>
