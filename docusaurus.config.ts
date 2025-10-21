7ab3fa2ec7716419ea599b2a824eb4cbc21c5062
Ben Allfree <ben@benallfree.com>
2025-10-21 03:29:36 -0700

    add GitHub Discussions link to the navigation bar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -123,6 +123,12 @@ const config: Config = {
           "aria-label": "GitHub",
         },
         {
+          href: "https://github.com/orgs/MeshEnvy/discussions",
+          html: '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"/></svg>',
+          position: "right",
+          "aria-label": "GitHub Discussions",
+        },
+        {
           href: "https://www.youtube.com/@meshenvyofficial",
           html: '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>',
           position: "right",
